10277ff84e04ecb53bab13d82e5dae43af45a5a3
Nuad106404 <93254468+Nuad106404@users.noreply.github.com>
2025-08-21 15:21:22 +0700

    Add PWA implementation and install missing dependencies

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-4c29871c'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.iusrsku7rag"
+    "revision": "0.veu64b5iek8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
